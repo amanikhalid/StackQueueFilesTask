a1510040b0bf9ee4ad0bc3808a0b1b3b1cacf082
Amani Khalid <amanikhalidd@gmail.com>
2025-04-21 15:15:10 +0400

    Added Reverse a string using stack

--- Program.cs
+++ Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            //Reverse a string using stack
+            Console.WriteLine("Enter a word to reverse:");
+            string word = Console.ReadLine();
+
+
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in word)
+            {
+                stack.Push(c);
+            }
+            string reversedStr = "  ";
+            while (stack.Count > 0)
+            {
+                reversedStr += stack.Pop();
+            }
+            Console.WriteLine("Reversed string: " + reversedStr);
+
             
 
 
